cce980d60bea48362c1f6fe335fa7ee8e5c9dd5c
sipian <harshaga97@gmail.com>
2017-04-21 17:08:29 +0530

    add evaluate minmax

--- test.pl
+++ test.pl
@@ -13,25 +13,26 @@ startGame:- chance(['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y'], 'x').
 chance(Board, 'x'):- 
     displayTicTaToe(Board), 
     (
-     checkIfWon(Board, 'x'), !, write("THE PLAYER HAS WON AGAINST THE COMPUTER\n");
-     checkIfWon(Board, 'o'), !, write("THE COMPUTER HAS WON AGAINST THE PLAYER\n");
-     neg(checkIfMovesLeft(Board)), !, write("THE GAME IS A DRAW.\n");
-     write("Make Your Move : "), nl, read(Input), nl, replace(Board, Input, 'x', NewBoard), chance(NewBoard, 'o')
+     checkIfWon(Board, 'x'), !, write('THE PLAYER HAS WON AGAINST THE COMPUTER\n');
+     checkIfWon(Board, 'o'), !, write('THE COMPUTER HAS WON AGAINST THE PLAYER\n');
+     neg(checkIfMovesLeft(Board)), !, write('THE GAME IS A DRAW.\n');
+     write('Make Your Move : '), nl, read(Input), nl, replace(Board, Input, 'x', NewBoard), chance(NewBoard, 'o')
     ).
     
 chance(Board, 'o'):- 
     displayTicTaToe(Board), 
     (
-     checkIfWon(Board, 'x'), !, write("THE PLAYER HAS WON AGAINST THE COMPUTER\n");
-     checkIfWon(Board, 'o'), !, write("THE COMPUTER HAS WON AGAINST THE PLAYER\n");
-     neg(checkIfMovesLeft(Board)), !, write("THE GAME IS A DRAW.\n");
-     write("Make Your Goddamn Move : "), nl, read(Input), nl, replace(Board, Input, 'o', NewBoard), chance(NewBoard, 'x')
+     checkIfWon(Board, 'x'), !, write('THE PLAYER HAS WON AGAINST THE COMPUTER\n');
+     checkIfWon(Board, 'o'), !, write('THE COMPUTER HAS WON AGAINST THE PLAYER\n');
+     neg(checkIfMovesLeft(Board)), !, write('THE GAME IS A DRAW.\n');
+     write('Make Your Goddamn Move : '), nl, read(Input), nl, replace(Board, Input, 'o', NewBoard), chance(NewBoard, 'x')
     ).
     
      % findBestMove(Board, Position), replace(Board, Position, 'o', NewBoard), chance(NewBoard, 'x')
 
 
 % Minmax Algo.
+
 
 evaluate(Board, Answer):-
     (
@@ -45,14 +46,57 @@ evaluate(Board, Answer):-
     ).
 
 
+minimax(Board, true, Score):-
+    (
+     evaluate(Board, 10), !, Score = 10;
+     evaluate(Board, -10), !, Score = -10;
+     neg(checkIfMovesLeft(Board)), !, Score = 0;
+     Best is -1000, functionMaxPlayer(Board, 0, Best), Score = Best
+    ).
+
 minimax(Board, false, Score):-
     (
      evaluate(Board, 10), !, Score = 10;
      evaluate(Board, -10), !, Score = -10;
      neg(checkIfMovesLeft(Board)), !, Score = 0;
-     Best = -1000, 
-     
-    )
+     functionMinPlayer(Board, 0, Best), Score = Best
+    ).
+
+functionMaxPlayer(Board, 9, -1000).
+functionMaxPlayer(Board, Index, Best) :-
+    (
+        nth(Index, Board, 'y'), !, replace(Board, Index, 'o', NewBoard), minimax(NewBoard, false, TempScore), max(Best, TempScore, NewScore), replace(NewBoard, Index, 'y', OldBoard), NextIndex is Index+1, functionMaxPlayer(OldBoard, NextIndex, NewScore), Best = NewScore;
+        
+        NextIndex is Index+1, functionMaxPlayer(Board, NextIndex, NewBest), Best = NewBest;
+    ).
+
+
+
+functionMinPlayer(Board, 9, 1000).
+functionMinPlayer(Board, Index, Best) :-
+    (
+        nth(Index, Board, 'y'), !, replace(Board, Index, 'o', NewBoard), minimax(NewBoard, true, TempScore), min(Best, TempScore, NewScore), replace(NewBoard, Index, 'y', OldBoard), NextIndex is Index+1, functionMinPlayer(OldBoard, NextIndex, NewScore), Best = NewScore;
+        
+        NextIndex is Index+1, functionMinPlayer(Board, NextIndex, NewBest), Best = NewBest;
+    ). 
+
+
+
+
+
+recursiveFunctionBestMove(Board, 9, -1000, 0).
+recursiveFunctionBestMove(Board, Index, Best, Chance) :-
+    (
+        nth(Index, Board, 'y'), !, NextIndex is Index+1, recursiveFunctionBestMove(Board, NextIndex, TempScore, NextChance), replace(Board, Index, 'o', NewBoard), minimax(NewBoard, false, TempScore), replace(NewBoard, Index, 'y', OldBoard), TempScore > Best, !, Chance = Index, N,  ;
+
+        nth(Index, Board, 'y'), !, replace(Board, Index, 'o', NewBoard), minimax(NewBoard, false, TempScore), replace(NewBoard, Index, 'y', OldBoard), TempScore <= Best, !, NextIndex is Index+1, recursiveFunctionBestMove(OldBoard, NextIndex, Best, NextChance) Chance = NextChance;
+        
+        NextIndex is Index+1, recursiveFunctionBestMove(Board, NextIndex, Best, NextChance), Chance = NextChance;
+    ). 
+    
+    
+    
+findBestMove(Board) :-
     
 
 % Predicates for checking winning
@@ -84,6 +128,11 @@ replace([_|T], 0, X, [X|T]).
 replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
 replace(L, _, _, L).
 
+max(A,B,Z):- A >= B, !, Z = A.
+max(A,B,Z):- A < B, Z = B.
+
+min(A,B,Z):- A =< B, !, Z = A.
+min(A,B,Z):- A > B, Z = B.
 
 
 
@@ -102,14 +151,14 @@ replace(L, _, _, L).
 
 
     displayTicTaToe([X1, X2, X3, X4, X5, X6, X7, X8, X9]):- 
-displayEmptyLine, write(" "), write(X1), write(" |"), write(" "), write(X2), write(" |"), write(" "), write(X3), displayBorderLine, 
-displayEmptyLine, write(" "), write(X4), write(" |"), write(" "), write(X5), write(" |"), write(" "), write(X6), displayBorderLine, 
-displayEmptyLine, write(" "), write(X7), write(" |"), write(" "), write(X8), write(" |"), write(" "), write(X9), write("\n"), displayEmptyLine.
+displayEmptyLine, write(' '), write(X1), write(' |'), write(' '), write(X2), write(' |'), write(' '), write(X3), displayBorderLine, 
+displayEmptyLine, write(' '), write(X4), write(' |'), write(' '), write(X5), write(' |'), write(' '), write(X6), displayBorderLine, 
+displayEmptyLine, write(' '), write(X7), write(' |'), write(' '), write(X8), write(' |'), write(' '), write(X9), write('\n'), displayEmptyLine.
 
 
     
-    displayEmptyLine:- write("   |   |   \n").
-    displayBorderLine:- write("\n___|___|___\n").
-    displayCharacterLine(X, Pos):- write(" "), write(X), write(" ").
+    displayEmptyLine:- write('   |   |   \n').
+    displayBorderLine:- write('\n___|___|___\n').
+    displayCharacterLine(X, Pos):- write(' '), write(X), write(' ').
     
     
